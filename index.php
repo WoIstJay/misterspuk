e85597ed53a9cce75302525338819ef82fb9174e
WoIstJay <83090727+WoIstJay@users.noreply.github.com>
2021-07-01 21:24:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <title>Links Mister_Spuk</title>
     <link href="https://bot-1.deutschlandmaps.repl.co/style.css" rel="stylesheet" type="text/css">
-    eta content="Mister Spuk" property="og:title">
+    <meta content="Mister Spuk" property="og:title">
   <meta content="Links Mister Spuk" property="og:description">
     
     
