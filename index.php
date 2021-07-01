434d4b7ad156812063ebde75d69937e5d578a85b
WoIstJay <83090727+WoIstJay@users.noreply.github.com>
2021-07-01 21:20:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,13 @@
 <html lang="en">
   <head>
     <title>Links Mister_Spuk</title>
-    
+    <link href="https://bot-1.deutschlandmaps.repl.co/style.css" rel="stylesheet" type="text/css">
     
     
     
     </head>
+  <body>
         
         
-        
+   </body>
   </html>
